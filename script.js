ba0a4739e9a25f0a47ce313e8cdc4397dc70b3a3
nikita <dashgerot@gmail.com>
2025-01-11 18:55:33 +0700

    adding my files to my repository

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const phoneLink = document.querySelector(".phone-link");
+
+  if (phoneLink) {
+    const isMobile = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
+
+    if (!isMobile) {
+      phoneLink.removeAttribute("href");
+      phoneLink.style.cursor = "default";
+    }
+  }
+
+  function addClassOnIntersect(className, targetClass, threshold = 0.5) {
+    function onEntry(entry) {
+      entry.forEach((change) => {
+        if (change.isIntersecting) {
+          change.target.classList.add(className);
+        }
+      });
+    }
+
+    const options = {
+      threshold: [threshold],
+    };
+
+    const observer = new IntersectionObserver(onEntry, options);
+    const elements = document.querySelectorAll(targetClass);
+
+    for (let elm of elements) {
+      observer.observe(elm);
+    }
+  }
+
+  addClassOnIntersect("images__background", ".hero__body");
+  addClassOnIntersect("hero_title", ".description__container");
+  addClassOnIntersect("description__header", ".achievements__items");
+  addClassOnIntersect("items", ".social__body");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openButton = document.getElementById("openMenuButton");
+  const closeButton = document.getElementById("closeMenuButton");
+  const dialog = document.getElementById("mobileOverlay");
+
+  const openMenu = () => dialog.showModal();
+  const closeMenu = () => dialog.close();
+
+  openButton.addEventListener("click", openMenu);
+  dialog.addEventListener("click", closeMenu);
+  closeButton.addEventListener("click", closeButton);
+});
+
